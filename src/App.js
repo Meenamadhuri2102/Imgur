eb95fdc28d1b4d0725b015ac999306c3e1e21245
Meenamadhuri2102 <74135688+Meenamadhuri2102@users.noreply.github.com>
2021-06-18 16:49:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavbarImgur from './components/navbar';
+
+function App() {
+  return (
+    <div className='App'>
+    <NavbarImgur/>
+    </div>
+  );
+}
+
+
+export default App;
